f2d6cc6228c0b5487f35bb31c3055d56e656841d
wangyang <398733146@qq.com>
2014-09-17 16:29:15 +0800

    init

---
+++ logisticsapp/src/test/java/com/incito/logistics/pages/LoginPage.java
@@ -0,0 +1,5 @@
+package com.incito.logistics.pages;
+
+public class LoginPage {
+
+}
---
+++ logisticsapp/src/test/java/com/incito/logistics/testcase/login/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+package com.incito.logistics.testcase.login;
